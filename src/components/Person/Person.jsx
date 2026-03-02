151abb2d5a8ddcbfad3f913e4c094a49c0384149
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-03-02 05:29:28 +0200

    task done, looks ok, not all tests passing

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,9 @@
-// export const Person = ({ person }) => ();
+import { Partner } from '../Partner';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
+    <Partner other={person.partnerName} me={person.sex} />
+  </section>
+);
