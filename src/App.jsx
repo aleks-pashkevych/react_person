18e98c7c0215717eb27297f79c78e28148da8c78
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-03-03 06:00:33 +0200

    corrected some mistakes

--- src/App.jsx
+++ src/App.jsx
@@ -26,22 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person
-      name={misha.name}
-      age={misha.age}
-      sex={misha.sex}
-      partnerName={misha.partnerName}
-    />
-    <Person
-      name={olya.name}
-      age={olya.age} sex={olya.sex}
-      partnerName={olya.partnerName}
-    />
-    <Person
-      name={alex.name}
-      age={alex.age}
-      sex={alex.sex}
-      partnerName={alex.partnerName}
-    />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,13 @@
 import { Partner } from '../Partner';
 
-export const Person = ({ name, age, sex, partnerName }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {name}</h2>
-    {age ? <p className="Person__age">I am {age}</p> : ''}
-    <Partner other={partnerName} me={sex} />
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      <Partner other={partnerName} me={sex} />
+    </section>
+  );
+};
